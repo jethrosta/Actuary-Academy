9dce64d62d07a4686e6f9ac6cd25121d46198ff6
dandyZicky <dandyzicky1@gmail.com>
2023-10-28 20:42:45 +0700

    move static assets to public/src/assets

--- Front-end/src/main.js
+++ Front-end/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 
-import './assets/main.css'
+import './main.css'
 import '@splidejs/vue-splide/css';
 
 const app = createApp(App)
